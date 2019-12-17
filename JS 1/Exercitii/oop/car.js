6783dd05a38ea237a89b54993f80e456989f0d3e
JavaScript <javascript@Admins-iMac-162.local>
2019-12-17 17:57:19 +0200

    mod 1

---
+++ JS 1/Exercitii/oop/car.js
@@ -0,0 +1,14 @@
+var car= {
+    make: "Ford",
+    model: "Mustang",
+    year: 2019,
+    color: "red",
+    passengers: 4,
+    mileage: 2000,
+    stop: function () {
+
+    },
+    start: function () {
+
+    },
+}
