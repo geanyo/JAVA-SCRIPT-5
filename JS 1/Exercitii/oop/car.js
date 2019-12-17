7f89a1fc266b62c5a02cb76c78ee30b9ff234ccb
JavaScript <javascript@Admins-iMac-162.local>
2019-12-17 18:29:39 +0200

    mod 2

--- JS 1/Exercitii/oop/car.js
+++ JS 1/Exercitii/oop/car.js
@@ -1,14 +1,34 @@
-var car= {
-    make: "Ford",
-    model: "Mustang",
-    year: 2019,
-    color: "red",
-    passengers: 4,
-    mileage: 2000,
-    stop: function () {
+var car = {
+  make: "Ford",
+  model: "Mustang",
+  year: 2019,
+  color: "red",
+  passengers: 4,
+  mileage: 2000,
+  engineIsOn: false,
+  stop: function() {
+    if (this.engineIsOn) {
+      this.engineIsOn = false;
+      console.log("Car stopped");
+    } else {
+      console.log("Car is already stopped");
+    }
+  },
+  start: function() {
+    if (!this.engineIsOn) {
+      this.engineIsOn = true;
+      console.log("Car started");
+    } else {
+      console.log("Car is already started");
+    }
+  }
+};
 
-    },
-    start: function () {
+console.log(car.year);
 
-    },
-}
+car.mileage = 5600;
+console.log(car.mileage);
+
+car.start();
+car.stop();
+car.stop();
