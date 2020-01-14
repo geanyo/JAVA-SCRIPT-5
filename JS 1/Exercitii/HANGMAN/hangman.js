633d56f97b53ab69d1a4562e82a0d822b7ed31f6
JavaScript <javascript@Admins-iMac-162.local>
2020-01-14 20:51:17 +0200

    com1

--- JS 1/Exercitii/HANGMAN/hangman.js
+++ JS 1/Exercitii/HANGMAN/hangman.js
@@ -12,7 +12,7 @@ var game= {
     },
     tryLetter: function(letter) {
         this.letters.push(letter);
-        if(!this.word.split('').includes(letter)) {
+        if(!this.word.includes(letter)) {
             this.triesLeft--;
             // if(!this.word.split('').includes(letter)) {
             //     this.triesLeft--; DACA S-A APASAT PE O TASTA, ATUNCI SCADE NUMARUL DE INCERCARI --- 6,5,4,3,2,1,0
@@ -22,7 +22,46 @@ var game= {
         return this.triesLeft== 0;
     },
     isWinner: function() {
-        
+        return this.word.split('')
+                        .every(letter => this.letters.includes(letter))
     }
 }
+
+function renderLetters() {
+    document.getElementById("letters").innerText= game.letters.join(' ');
+}
+function renderWord() {
+    let text= game.word.split('')
+                        .map(letter => game.letters.includes(letter) ? letter: '_')
+                        .join(' ');
+    document.getElementById("word").innerText= text;
+}
+function renderDrawing() {
+    let parts= ['head', 'body', 'left_arm', 'right_arm', 'left_leg', 'right_leg'];
+    let visible= parts.slice(0, 6 - game.triesLeft);
+    let invisible=parts.slice(6 - game.triesLeft);
+    visible.forEach(name => document.getElementById(name).style.visibility= 'visible');
+}
+function renderView() {
+    renderLetters();
+    renderWord();
+    renderDrawing();
+}
+
+// game.word= 'APPLE';
+// game.letters= ['A', 'E'];
+// game.triesLeft= 6;
+
+// renderView();
+
+window.addEventListener('keydown', function(event) {
+    if (game.isDead() || game.isWinner()) return;
+    game.tryLetter(event.key);
+    renderView();
+});
+game.start();
+renderView();
+
+
+
 
