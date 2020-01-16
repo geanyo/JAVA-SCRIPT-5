f7a0be3a33dd3ced8c0d82264793edbc13909f7e
JavaScript <javascript@Admins-iMac-162.local>
2020-01-16 20:43:06 +0200

    COM 1

---
+++ JS 1/Exercitii/storage/storage.js
@@ -0,0 +1,17 @@
+name= localStorage.getItem("myname");
+if(name != null) {
+    document.getElementById("form").style.display= "none";
+    let text= `Hello, ${name}!`;
+    document.getElementById("hello").innerText= text;
+}
+
+document.querySelector("input[type= button]")
+.addEventListener("click", hello);
+
+function hello() {
+    document.getElementById("form").style.display= "none";
+    let name= document.getElementById("name").value;
+    let text= `Hello, ${name}!`;
+    localStorage.setItem("myname", name);
+    document.getElementById("hello").innerText= text;
+}        
