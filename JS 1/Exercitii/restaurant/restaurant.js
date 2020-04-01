ae0bbca03692ced29394f87967cdf274d2701d03
Real Partener <grafica1@Reals-iMac.local>
2020-04-01 10:53:40 +0300

    mod 4

--- JS 1/Exercitii/restaurant/restaurant.js
+++ JS 1/Exercitii/restaurant/restaurant.js
@@ -1,16 +1,16 @@
-function addlisteners() {
+function addListeners() {
     document.querySelector('.hamburger').addEventListener('click', toggleMenu);
 }
 
 function toggleMenu() {
     // get menu element
-    document.querySelector('.menu');
+    let menu= document.querySelector('.menu');
     //if menu is visible hide it, else show it
     if(menu.classList.contains('visible')) {
         menu.classList.remove('visible');
-    } else() {
+    } else {
         menu.classList.add('visible');
        } 
 }
 
-window.addEventListener('load', addlisteners);
+window.addEventListener('load', addListeners);
