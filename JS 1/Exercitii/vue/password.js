f1d99b4e8dad1dfb47d4945d617040eacd864d97
Real Partener <grafica1@Reals-iMac.local>
2020-04-09 15:35:57 +0300

    mod2

--- JS 1/Exercitii/vue/password.js
+++ JS 1/Exercitii/vue/password.js
@@ -18,25 +18,45 @@ var app= new Vue({
         evaluateScore: function() {
             // definim noul scor initial egal cu 0
             let newScore= 0;
+            
             // evaluam fiecare conditie dintre cele 5 - definim cele 5 conditii
             // adaugam 1 la noul scor pentru fiecare conditie adevarata
-            if(this.hasSmallLetter()) {
-                newScore += 1;
-            } 
-            if(this.hasUpperLetter()) {
-                newScore += 1;
-            } 
-            if(this.hasDigit()) {
-                newScore += 1;
-            } 
-            if(this.hasSpecialChar()) {
-                newScore += 1;
-            } 
-            if(this.isLongEnough()) {
-                newScore += 1;
-            } 
+            
+            newScore += this.hasSmallLetter();
+            newScore += this.hasUpperLetter();
+            newScore += this.hasDigit();         
+            newScore += this.hasSpecialChar();
+            newScore += this.isLongEnough();          
+     
+            
+            // newScore= [
+            //     this.hasSmallLetter,
+            //     this.hasUpperLetter
+            //     this.hasDigit
+            //     this.hasSpecialChar                   ALTERNATIVA 1
+            //     this.isLongEnough
+            // ].reduce((score, func) => score + func(), 0);
+
+            // if(this.hasSmallLetter()) {
+            //     newScore += 1;
+            // } 
+            // if(this.hasUpperLetter()) {
+            //     newScore += 1;
+            // } 
+            // if(this.hasDigit()) {                      ALTERNATIVA 2
+            //     newScore += 1;
+            // } 
+            // if(this.hasSpecialChar()) {
+            //     newScore += 1;
+            // } 
+            // if(this.isLongEnough()) {
+            //     newScore += 1;
+            // } 
+
+
             // inlocuim vechiul scor cu noul scor
             this.score= newScore;
+
             // inlocuim mesajul cu noul mesaj asociat noului scor
             this.message= this.messages[newScore];
         },
