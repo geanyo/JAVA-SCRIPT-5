429e08a249e854deb0bfc041d33da9e83e934120
Real Partener <grafica1@Reals-iMac.local>
2020-04-09 20:31:47 +0300

    mod 6

--- JS 1/Exercitii/xz/xz.js
+++ JS 1/Exercitii/xz/xz.js
@@ -1,37 +1,41 @@
 var activePlayer= 'x';
 
-$('.table div'). click(makeMove);
-
-function makeMove() {
-    $(this).text(activePlayer)
-            .addClass(activePlayer== 'x' ? 'x' : 'z')
-            .unbind('click', makeMove);
-    activePlayer= activePlayer== 'x' ? 'o' : 'x';
-    checkEndgame();
-}
-function checkEndgame() {
-    let cells= $('.table div').toArray();
-    checkComplete(cells.filter((value, index) => index<3));
-    checkComplete(cells.filter((value, index) => index>2 && index<6));
-    checkComplete(cells.filter((value, index) => index>=5));
-    checkComplete(cells.filter((value, index) => index % 3 ==0));
-    checkComplete(cells.filter((value, index) => index % 3 ==1));
-    checkComplete(cells.filter((value, index) => index % 3 ==2));
-    checkComplete(cells.filter((value, index) => index % 4 ==0));
-    checkComplete(cells.filter((value, index) => [2, 4, 6].includes(index)));
-}
-
-/**
- * 
- * @param {Array} cells 
- */
-
- // JS DOC----DOCUMENTATIE DE TIP COD
-
-function checkComplete(cells) {
-    if(cells.every(cell => $(cell).text() == 'x')
-    || cells.every(cell => $(cell).text() == 'o')
-    ) {
-        $('.end').addClass('visible');
+var app= new Vue({
+    el: "#app",
+    data: {
+        table: [
+            '', '', '', '', '', '', '', '', '', 
+        ],
+        activePlayer: "X",
+        finished: false
+    },
+    methods: {
+        // index reprezinta celula din tabel pe care se face mutarea
+        makeMove(index) {
+            // (guard clause) ----- verificam daca este ceva pe acea casuta/celula
+            if (this.table[index] != '') return;
+            //  folosim splice pentru a permite reactivitatea proprietatii tabelului
+            this.table.splice(index, 1, this.activePlayer);
+            // schimbam jucatorul activ si verificam daca s-a terminat jocul dupa aceasta mutare
+            this.activePlayer= this.activePlayer== 'x' ? 'o' : 'x';
+            this.checkFinished();
+        },
+        checkFinished() {
+            this.checkComplete(this.table.filter((value, index) => index<3));
+            this.checkComplete(this.table.filter((value, index) => index>2 && index<6));
+            this.checkComplete(this.table.filter((value, index) => index>=5));
+            this.checkComplete(this.table.filter((value, index) => index % 3 ==0));
+            this.checkComplete(this.table.filter((value, index) => index % 3 ==1));
+            this.checkComplete(this.table.filter((value, index) => index % 3 ==2));
+            this.checkComplete(this.table.filter((value, index) => index % 3 ==2));
+            this.checkComplete(this.table.filter((value, index) => index % 4 ==0));
+            this.checkComplete(this.table.filter((value, index) => [2, 4, 6].includes(index)));
+        },
+        checkComplete(cells) {
+            if(cells.every(cell => cell == 'x')
+            || cells.every(cell => cell == 'o')
+            ) {
+                this.finished= true;
+            }
     }
-}
+})
