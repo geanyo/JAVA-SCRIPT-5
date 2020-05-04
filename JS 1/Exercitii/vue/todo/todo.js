e817f01cc5218b3a52045fe31ad3dcbe7a38f194
Real Partener <grafica1@Reals-iMac.local>
2020-05-04 13:20:26 +0300

    mod 3

--- JS 1/Exercitii/vue/todo/todo.js
+++ JS 1/Exercitii/vue/todo/todo.js
@@ -1,82 +1,76 @@
-// class Task {
-//     constructor(title) {
-//         this.title = title;
-//         this.isChecked = false;
-//     }
-//     switch() {
-//         this.isChecked = !this.isChecked;
-//     }
-// }
+class Todo {
+    constructor(title) {
+        this.title = title;
+        this.isDone = false;
+    }
+    switchDone() {
+        this.isDone = !this.isDone;
+    }
+}
 
-// Vue.component("todo-item", {
-//     // template
-//     template: `<li class="todo-item">
-//                 <div class="title" v-bind:class="{ checked: isChecked }">{{ title }}</div>
-//                 <div class="controls">
-//                     <div class="check" @click="$emit('checked')">✓</div>
-//                     <div class="remove" @click="$emit('deleted')">✕</div>
-//                 </div>
-//             </li>`,
-//     // proprietati
-//     props: ["title", "isChecked"],
-// });
+Vue.component('task', {
+    template: `
+        <li :class="{ done: isDone }" class="task">  
+            <div class="title">{{ title }}</div>
+            <div class="controls">
+                <div class="check" @click="$emit('checked')">&#x2713;</div>
+                <div class="delete" @click="$emit('deleted')">&#x2715;</div>
+            </div>
+        </li>
+    `,
+    props: ['title', 'isDone']
+});
 
+Vue.component('todo-list', {
+    template: `
+    <div class="todo-list">
+        <h1>To do:</h1>
+        <div class="filters">
+            <label>Show done:</label>
+            <input type="checkbox" v-model="showDone">
+        </div>
+        <ul>
+            <task
+                v-if="showDone || !todo.isDone" 
+                v-for="(todo, index) in todos"
+                :title="todo.title"
+                :is-done="todo.isDone"
+                @checked="checkTodo(index)"
+                @deleted="deleteTodo(index)">
+            </task>
+        </ul>
+        <div class="new_task">
+                Task:
+                <input type="text" v-model="task" @keyup.enter="saveTodo">
+            <div class="save_task"><button @click="saveTodo">Save item</button></div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            todos: [
+                new Todo('Something to do')
+            ],
+            task: '',
+            showDone: true
+        }
+    },
+    methods: {
+        saveTodo() {
+            // adaugam valoarea din task in lista de todo
+            this.todos.push(new Todo(this.task));
+            // resetam task-ul
+            this.task = '';
+        },
+        checkTodo(index) {
+            this.todos[index].switchDone();
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        }
+    }
+});
 
-// Vue.component("todo-list", {
-//     template: `
-//       <div class="todo-list">
-//         <h1>To do:</h1>
-//         <div class="options">
-//             <label for="chow-checked">Show checked:</label>
-//             <input class="show-checked" type="checkbox" v-model="showChecked" />
-//         </div>
-//         <ul>
-//             <todo-item
-//             v-if="showChecked || !task.isChecked"
-//             v-for="(task, index) in todos"
-//             :key="index"
-//             :title="task.title"
-//             :is-checked="task.isChecked"
-//             @checked="switchTask(index)"
-//             @deleted="deleteTask(index)"
-//             >
-//             </todo-item>
-//         </ul>
-//         <div class="new_task">
-//             Task:
-//             <input type="text" v-model="task" @keyup.enter="saveTask" />
-//             <div class="save_task">
-//             <button @click="saveTask">Save item</button>
-//             </div>
-//         </div>
-//       </div>
-//     `,
-//     data() {
-//         return {
-//             todos: [new Task("Something to do")],
-//             task: "",
-//             showChecked: true
-//         }
-//     },
-//     methods: {
-//         saveTask() {
-//             // adaugam valoarea din task in lista de todo
-//             this.todos.push(new Task(this.task));
-//             // resetam task-ul
-//             this.task = "";
-//         },
-//         switchTask(index) {
-//             this.todos[index].switch();
-//         },
-//         deleteTask(index) {
-//             this.todos.splice(index, 1);
-//         },
-//         switchShowChecked() {
-//             this.showChecked = !this.showChecked;
-//         }
-//     },
-// })
-
-// var app = new Vue({
-//     el: "#app",
-// });
+var app = new Vue({
+    el: '#app'
+});
