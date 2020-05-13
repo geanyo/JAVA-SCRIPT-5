4a93272a1c0e845ee594470bfa68dbce930e2ca0
Real Partener <grafica1@Reals-iMac.local>
2020-05-13 19:01:29 +0300

    m2

---
+++ JS 1/Exercitii/RECAPITULARE JS/JS1 1-9/first.js
@@ -0,0 +1,33 @@
+var name = "Geanina";
+// var message = "Hello Java Script!";        
+var message = "Hello " + name + "!";
+var timeout = 3000;
+var isNight = false;
+var isMorning = true;
+var nothing = null;
+
+// var message = "Hello Java Script!"; -------- STRING
+// var timeout = 3000; ------- NUMERIC
+// var isNight = true; ------- BOOLEAN
+// var nothing; ------- CHIAR DACA NU ARE TRECUTA NICIO VALOARE, EA ARE O VALOARE UNDEFINED
+
+// VARIABILELE SUNT DENUMIRI GANDITE PENTRU VALORI IN MEMORIE (CONTAINERE TEMPORARE IN CARE SE DEPOZITEAZA VALORI)
+
+// \" ------- CHARACTER ESCAPE -------- CAND VREM SA FOLOSIM GHILIMELE INTR-UN STRING (intr-un tex "\"Hello Java" Script!")
+
+if(isNight) {
+    alert("Noapte buna!"); 
+} else if(isMorning) {
+    alert("Buna dimineata!");
+} else {
+    alert("Buna ziua!");
+}
+
+
+// setTimeout(sayHello, timeout);
+// function sayHello() {
+// alert(message);
+// // console.log(message);
+// }
+
+
