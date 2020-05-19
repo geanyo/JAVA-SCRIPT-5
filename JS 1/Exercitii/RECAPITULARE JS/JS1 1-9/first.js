2013794b89d67308d1e3b7e247c99b6b56570d1b
Real Partener <grafica1@Reals-iMac.local>
2020-05-19 13:14:24 +0300

    1

--- JS 1/Exercitii/RECAPITULARE JS/JS1 1-9/first.js
+++ JS 1/Exercitii/RECAPITULARE JS/JS1 1-9/first.js
@@ -3,8 +3,27 @@ var name = "Geanina";
 var message = "Hello " + name + "!";
 var timeout = 3000;
 var isNight = false;
-var isMorning = true;
+var isMorning = false;
 var nothing = null;
+
+var date = new Date();
+var hour = date.getHours();
+
+if(hour >= 21 && hour <= 6) {
+    alert("Noapte buna!");
+    alert("Somn usor!");
+} else if(hour <= 11) {
+    alert("Buna dimineata!");
+} else if(hour <= 17) {
+    alert("Buna ziua!");
+} else {
+    alert("Buna seara!");
+}
+
+
+
+// var hour = date.getHours(); ------ GETHOURS ia ora din zi
+
 
 // var message = "Hello Java Script!"; -------- STRING
 // var timeout = 3000; ------- NUMERIC
@@ -14,20 +33,3 @@ var nothing = null;
 // VARIABILELE SUNT DENUMIRI GANDITE PENTRU VALORI IN MEMORIE (CONTAINERE TEMPORARE IN CARE SE DEPOZITEAZA VALORI)
 
 // \" ------- CHARACTER ESCAPE -------- CAND VREM SA FOLOSIM GHILIMELE INTR-UN STRING (intr-un tex "\"Hello Java" Script!")
-
-if(isNight) {
-    alert("Noapte buna!"); 
-} else if(isMorning) {
-    alert("Buna dimineata!");
-} else {
-    alert("Buna ziua!");
-}
-
-
-// setTimeout(sayHello, timeout);
-// function sayHello() {
-// alert(message);
-// // console.log(message);
-// }
-
-
