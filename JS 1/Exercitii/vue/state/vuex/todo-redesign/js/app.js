623402884d302ae0714f8f30c8b4e33892a59ad5
Real Partener <grafica1@Reals-iMac.local>
2020-05-25 20:16:00 +0300

    7

---
+++ JS 1/Exercitii/vue/state/vuex/todo-redesign/js/app.js
@@ -0,0 +1,9 @@
+
+import "https://unpkg.com/vue/dist/vue.js"
+import "https://unpkg.com/vuex/dist/vuex.js"
+import './component/task.js';
+import './component/todo-list.js';
+
+var app = new Vue({
+    el: '#app'
+});
