bd8144002148e329a01ac1abf6271fa685e89a55
Real Partener <grafica1@Reals-iMac.local>
2020-05-25 18:12:22 +0300

    3

--- JS 1/Exercitii/vue/state/vuex/vuex.js
+++ JS 1/Exercitii/vue/state/vuex/vuex.js
@@ -2,6 +2,8 @@ import "https://unpkg.com/vue/dist/vue.js"
 import "https://unpkg.com/vuex/dist/vuex.js"
 import "https://unpkg.com/vue-router/dist/vue-router.js"
 
+Vue.use(Vuex)
+
 const store = new Vuex.Store({
     state: {
         count: 0
@@ -12,28 +14,45 @@ const store = new Vuex.Store({
     }
 })
 
+const PlusButton = {
+    template: `<button @click='increment'>+</button>`,
+    methods: {
+        increment: () => store.commit('increment')
+    }
+}
+
+const MinusButton = {
+    template: `<button @click='decrement'>-</button>`,
+    methods: {
+        decrement: () => store.commit('decrement')
+    }
+}
+
 new Vue({
     el: `#app`,
     store,
+    components: {PlusButton, MinusButton},
     template: `
         <div>
             <p>{{count}}</p>
             <p>
-                <button @click='increment'>+</button>
-                <button @click='decrement'>-</button>
+                <plus-button></plus-button>
+                <minus-button></minus-button>
             </p>
         </div>
     `,
     computed: {
         count() {
-            return store.state.count
+            return store.state.count  
+            // return this.$store.state.count   ------ SAU ASA
         }
-    },
-    methods: {
-//      ------     () =>    ------ NU PRIMIM NICIUN ARGUMENT, parametru zero
-// mai jos facem commit pe doua mutatii
-        increment: () => store.commit('increment'),
-        decrement: () => store.commit('decrement')
     }
 })
+//     methods: {
+// //      ------     () =>    ------ NU PRIMIM NICIUN ARGUMENT, parametru zero
+// // mai jos facem commit pe doua mutatii
+//         increment: () => store.commit('increment'),
+//         decrement: () => store.commit('decrement')
+//     }
+
 
