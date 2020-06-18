a8459795d77c282f41f8ac6df24129f01200527f
Geanina Stanculescu <geanyo@yahoo.com>
2020-06-18 16:49:55 +0300

    2

--- JS 1/Exercitii/callback/circle.js
+++ JS 1/Exercitii/callback/circle.js
@@ -1,3 +1,5 @@
+// METODA CALLBACK
+
 function go() {
     showCircle(150, 150, 100, div => {
         div.classList.add('message')
@@ -23,6 +25,42 @@ function showCircle(x, y, r, callback) {
         })
     }, 0)
 }
+
+
+
+// METODA PROMISIUNII
+
+// function go() {
+//     showCircleWithPromise(150, 150, 100).then(
+//     div => {
+//         div.classList.add('message')
+//         div.append('Hello world!')
+//         }
+//     )
+    
+// }
+// function showCircleWithPromise(x, y, r, callback) {
+//     let div = document.createElement('div')
+//     div.style.width = 0
+//     div.style.height = 0
+//     div.style.left = x + 'px'
+//     div.style.top = x + 'px'
+//     // astea sunt SETARILE INITIALE DE LA CARE FACE TRANZITIA
+//     div.className = 'circle'
+//     document.body.append(div)
+
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             div.style.width = r * 2 + 'px'
+//             div.style.height = r * 2 + 'px'
+//             div.addEventListener('transitionend', function handler() {
+//                 div.removeEventListener('transitionend', handler)
+//                 resolve(div)
+//             })
+//         }, 0)
+//     })
+
+// }
 
 
 
