4ee2d11c82c85ed56bc92a8c87ab4227180f985f
Geanina Stanculescu <geanyo@yahoo.com>
2020-06-19 17:23:42 +0300

    4

---
+++ JS 1/Exercitii/hanoi/hanoi.js
@@ -0,0 +1,45 @@
+function randomColor() {
+    return '#' + Math.random().toString(16).substri(-6);
+}
+
+class Disk {
+    constructor(size) {
+        this.size = size
+        this.color = randomColor()
+    }
+    biggerThan(anotherDisk) {
+        return this.size > anotherDisk.size
+    }
+}
+
+class Rod {
+    constructor(id, numberOfDisks, size) {
+        this.id = id
+        this.size = size
+        this.disks = []
+        createDisks(numberOfDisks)
+    }
+    createDisks(numberOfDisks) {
+        for(let size=numberOfDisks; size>0; size--) {
+            this.disks.push(new Disk(size))
+        }
+    }
+    put(disk) {
+        if (this.canPutDisk()) {
+            this.disks.push(disk)
+        } else {
+        throw new Error('Cannot put disk')
+        }
+    }
+    canPutDisk(disk) {
+        return this.isEmpty()
+        || this.topDisk().biggerThan(disk)
+    }
+    pop() {
+        if (!this.isEmpty()) {
+        return this.disks.pop()
+        } else {
+        new Error('Rod is empty')
+        }
+    }
+}
