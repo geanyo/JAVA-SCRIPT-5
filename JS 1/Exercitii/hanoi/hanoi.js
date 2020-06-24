36499b934fd467a10a3a3173500e983f2ee016d1
Geanina Stanculescu <geanyo@yahoo.com>
2020-06-24 17:40:41 +0300

    3

--- JS 1/Exercitii/hanoi/hanoi.js
+++ JS 1/Exercitii/hanoi/hanoi.js
@@ -1,5 +1,16 @@
+import 'http://unpkg.com/vue/dist/vue.js'
+import 'http://unpkg.com/vuex/dist/vuex.js'
+
+
 function randomColor() {
     return '#' + Math.random().toString(16).substri(-6);
+}
+
+class Move {
+    constructor(sourceRodId, targetRodId) {
+        this.sourceRodId = sourceRodId
+        this.targetRodId = targetRodId
+    }
 }
 
 class Disk {
@@ -53,13 +64,58 @@ class Rod {
                 C: new Rod('C', 0, numberOfDisks)
             }
         }
-        move(sourceRodId, targetRodId) {
-           let sourceRod  = this.rods(sourceRodId)
-           let targetRod = this.rods(targetRodId)
+        makeMove(move) {
+           let sourceRod  = this.rods(move.sourceRodId)
+           let targetRod = this.rods(move.targetRodId)
            if (!targetRod.canPutDisk(sourceRod.topDisk())) {
                throw new Error('Cannot put disk')
            }
            let disk = sourceRod.pop()
            targetRod.put(disk)
         }
+    }
+
+const store = new Vuex.Store({
+        state: {
+            game: new game(4),
+        },
+        mutations: {
+            makeMove: (state, move) => state.game.makeMove(move)
+        }
+    })
+
+    const rod = {
+        props: ['rod'],
+        template: `
+            <div class="rod"
+                :style="{ height: disksHeight(rod.size), width: diskWidth(rod.size) }"
+            >
+                <div v-for="disk in rod.disks"
+                    :key="disk.size"
+                    class="disks"
+                    :style="{ 
+                        height:diskHeight(1),
+                        width: diskWidth(disk.size),
+                        backgroundColor: disk.color
+                    }"
+                >
+                </div>
+            </div>
+        `,
+        methods: {
+            disksHeight: (disks) => disks * 2 + 'em',
+            diskWidth: (diskSize) => diskSize * 3 + 'em'
+        },
+    }
+
+    const game = {
+        template: `
+        <div class="game">
+            <rod v-for="rod in rods" :rod="rod" :key="rod.id"></rod>
+        </div>
+        `,
+        computed: {
+            rods: () => store.state.game.rods
+        },
+        components: {rod}
     }
