5fa4b1fac96df80d49e7743b2d8bbd8eba6c6268
Geanina Stanculescu <geanyo@yahoo.com>
2020-08-26 16:57:19 +0300

    1

--- JS 1/Exercitii/jquery/jqtest.js
+++ JS 1/Exercitii/jquery/jqtest.js
@@ -1,23 +1,40 @@
-$('input.first').click(changeButton);
-$('input.second').click(styleButton);
-
-
-// document.querySelector('.input').addEventListener('click', changeButton); ----- IN JAVA SCRIPT NATIVE
+$('input').click(changeButton);
 
 function changeButton() {
-    let newElem= $('<span>Hello world!</span>')
-    $('div').slideToggle()
-            .html(newElem);
+    $('div').hide();
+}
 
-    // $('div').text('You clicked!');  
-    // document.querySelector('div').style.visibility='hidden';
-}
-function styleButton() {
-    $('div').css({
-        'background-color': 'lime',
-        'top': '30vh'
-    });
-}
+
+
+
+
+
+
+
+
+
+
+
+// $('input.first').click(changeButton);
+// $('input.second').click(styleButton);
+
+
+// // document.querySelector('.input').addEventListener('click', changeButton); ----- IN JAVA SCRIPT NATIVE
+
+// function changeButton() {
+//     let newElem= $('<span>Hello world!</span>')
+//     $('div').slideToggle()
+//             .html(newElem);
+
+//     // $('div').text('You clicked!');  
+//     // document.querySelector('div').style.visibility='hidden';
+// }
+// function styleButton() {
+//     $('div').css({
+//         'background-color': 'lime',
+//         'top': '30vh'
+//     });
+// }
 // document.querySelector('div').style.backgroundColor='lime';
 
 // function changeButton() {
